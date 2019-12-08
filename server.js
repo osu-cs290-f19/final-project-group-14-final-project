47a41dd8ed63126cc458119a345a8fb5526bb2e0
Kevin Moy <moyke@flip2.engr.oregonstate.edu>
2019-12-08 01:01:53 -0800

    First update to 404.html and server.js; added nodemon

---
+++ server.js
@@ -0,0 +1,22 @@
+var path = require('path');
+var express = require('express');
+var exphbs = require('express-handlebars');
+var app = express();
+var port = process.env.PORT || 3000;
+
+app.get('/', function (req, res, next)
+{
+	res.status(200).sendFile(__dirname + '/public/index.html');
+});
+
+app.use(express.static('public'));
+
+app.get('*', function (req, res, next)
+{
+	res.status(404).sendFile(__dirname + '/public/404.html');
+});
+
+app.listen(port, function ()
+{
+	console.log('== Listening on port', port);
+});
