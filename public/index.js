bf8153e0f63408e2157ee55019f5c0ac240bdaa0
Kevin Moy <moyke@flip2.engr.oregonstate.edu>
2019-12-08 18:24:12 -0800

    Updated index.html, style.css, and index.js

--- public/index.js
+++ public/index.js
@@ -13,6 +13,7 @@ templatesTab.addEventListener('click', function ()
 	completedMadlibs.classList.add('hidden');
 	completedMadlibsTab.classList.remove('tab-clicked');
 	aboutUs.classList.add('hidden');
+	aboutUsTab.classList.remove('tab-clicked');
 });
 
 // Completed Mad Libs Tab
@@ -43,4 +44,36 @@ aboutUsTab.addEventListener('click', function ()
 	templatesTab.classList.remove('tab-clicked');
 	completedMadlibs.classList.add('hidden');
 	completedMadlibsTab.classList.remove('tab-clicked');
+});
+
+// Mad Lib Template
+var template = document.getElementsByClassName('template');
+var templateTitle = document.getElementsByClassName('template-title');
+var templatePostTitle = document.getElementsByClassName('template-post-title');
+var templatePostBackdrop = document.getElementById('template-post-backdrop');
+var templatePosts = document.getElementById('template-posts');
+
+for (var i = 0; i < template.length; i++)
+{
+	(function (index)
+	{
+		template[index].addEventListener('click', function ()
+		{
+			console.log('== Template clicked');
+			console.log('== Template Title:', templateTitle[index].textContent);
+
+			templatePostBackdrop.classList.remove('hidden');
+			templatePosts.classList.remove('hidden');
+			templatePostTitle[0].textContent = templateTitle[index].textContent;
+		});
+	})(i);
+}
+
+// Template Post Cancel Button
+var templatePostCancelButton = document.getElementById('template-post-cancel');
+
+templatePostCancelButton.addEventListener('click', function ()
+{
+	templatePostBackdrop.classList.add('hidden');
+	templatePosts.classList.add('hidden');
 });
